694c0f37eae161bd0f315689096d60961aa050ca
Saikat379 <rsaikat978@gmail.com>
2021-10-13 15:32:44 +0530

    DevOPs Code Completed

---
+++ src/test/java/loginTests/NumberComparison.java
@@ -0,0 +1,11 @@
+package loginTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NumberComparison {
+  @Test
+  public void intComparison() {
+	  Assert.assertTrue(5>4);
+  }
+}
